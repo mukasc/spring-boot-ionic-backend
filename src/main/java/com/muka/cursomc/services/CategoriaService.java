9da28f1480430e83f3710badeb3807b7143b233c
muka <mukasc@gmail.com>
2019-01-27 21:22:58 -0200

    Atualizando categoria com PUT

--- src/main/java/com/muka/cursomc/services/CategoriaService.java
+++ src/main/java/com/muka/cursomc/services/CategoriaService.java
@@ -29,6 +29,12 @@ public class CategoriaService {
 	} 
 	
 	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 	
