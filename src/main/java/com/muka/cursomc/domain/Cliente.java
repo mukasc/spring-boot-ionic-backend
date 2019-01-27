074ed47d633d102f02c48444441f59afb5f9f76f
muka <mukasc@gmail.com>
2019-01-27 17:50:43 -0200

    enum estadopagamento, pagamento, pagamento com boleto, pagamento com cartao, pedido, Mapeamento 1 para 1

--- src/main/java/com/muka/cursomc/domain/Cliente.java
+++ src/main/java/com/muka/cursomc/domain/Cliente.java
@@ -37,6 +37,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -105,6 +107,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,6 +139,8 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
