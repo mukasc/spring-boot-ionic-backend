0b83cedd1dd9d5d60274d7d9c2455b1e4488bb0c
muka <mukasc@gmail.com>
2019-01-31 21:44:34 -0200

    correcoes, update em catagoria service e construtor de pagamento

--- src/main/java/com/muka/cursomc/services/CategoriaService.java
+++ src/main/java/com/muka/cursomc/services/CategoriaService.java
@@ -41,10 +41,11 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
+		
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -54,7 +55,6 @@ public class CategoriaService {
 			throw new DataIntegrityException("Nao e possivel excluir uma categoria que possui produtos!");
 		}
 	}
-	
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
@@ -67,6 +67,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
