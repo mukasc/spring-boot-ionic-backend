29849fe067d5ce6fa731871b2d39b69f41248152
muka <mukasc@gmail.com>
2019-01-31 23:35:11 -0200

    Inserindo um novo cliente com post

--- src/main/java/com/muka/cursomc/resources/ClienteResource.java
+++ src/main/java/com/muka/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.muka.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.muka.cursomc.domain.Cliente;
 import com.muka.cursomc.dto.ClienteDTO;
+import com.muka.cursomc.dto.ClienteNewDTO;
 import com.muka.cursomc.services.ClienteService;
 
 @RestController
@@ -45,6 +48,14 @@ public class ClienteResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
--- src/main/java/com/muka/cursomc/services/ClienteService.java
+++ src/main/java/com/muka/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.muka.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,10 +12,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.muka.cursomc.domain.Cidade;
 import com.muka.cursomc.domain.Cliente;
-import com.muka.cursomc.domain.Cliente;
+import com.muka.cursomc.domain.Endereco;
+import com.muka.cursomc.domain.enums.TipoCliente;
 import com.muka.cursomc.dto.ClienteDTO;
+import com.muka.cursomc.dto.ClienteNewDTO;
+import com.muka.cursomc.repositories.CidadeRepository;
 import com.muka.cursomc.repositories.ClienteRepository;
+import com.muka.cursomc.repositories.EnderecoRepository;
 import com.muka.cursomc.services.exceptions.DataIntegrityException;
 import com.muka.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +29,12 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	//public Optional<Cliente> find(Integer id) {
 	//	Optional<Cliente> obj = repo.findById(id);
@@ -35,6 +48,14 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
 	} 
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;	
+	}
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -63,6 +84,22 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfcnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
