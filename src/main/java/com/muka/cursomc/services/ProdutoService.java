91e8360599cc133d7edf3ffa47615f4b32c1a0ff
muka <mukasc@gmail.com>
2019-02-03 16:23:55 -0200

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/muka/cursomc/services/ProdutoService.java
@@ -0,0 +1,49 @@
+package com.muka.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.muka.cursomc.domain.Categoria;
+import com.muka.cursomc.domain.Produto;
+import com.muka.cursomc.repositories.CategoriaRepository;
+import com.muka.cursomc.repositories.ProdutoRepository;
+import com.muka.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	//public Optional<Produto> find(Integer id) {
+	//	Optional<Produto> obj = repo.findById(id);
+		//return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName())); 
+		//if (obj == null) {
+		//	throw new ObjectNotFoundException("Objeto nao encontrado! " + id + ", Tipo: " + Produto.class.getName());
+		//}
+		//return obj;
+	//} 
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName())); 
+	} 
+	
+public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+	List<Categoria> categorias = categoriaRepository.findAllById(ids);
+	return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	
+	
+	
+	}
+	
+}
