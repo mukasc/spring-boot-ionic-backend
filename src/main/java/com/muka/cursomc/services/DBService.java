2378911c1528a40b56ef40a5ca580c5dd0d3b3da
muka <mukasc@gmail.com>
2019-02-09 13:52:53 -0200

    Salvando perfis de usuario na base de dados

--- src/main/java/com/muka/cursomc/services/DBService.java
+++ src/main/java/com/muka/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.muka.cursomc.domain.PagamentoComCartao;
 import com.muka.cursomc.domain.Pedido;
 import com.muka.cursomc.domain.Produto;
 import com.muka.cursomc.domain.enums.EstadoPagamento;
+import com.muka.cursomc.domain.enums.Perfil;
 import com.muka.cursomc.domain.enums.TipoCliente;
 import com.muka.cursomc.repositories.CategoriaRepository;
 import com.muka.cursomc.repositories.CidadeRepository;
@@ -129,13 +130,19 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "mukasc@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Murillo", "muka.phoenix@gmail.com", "44697113063", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("999692588","99549199"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "382220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Av. Floriano", "2106", null, "Centro", "88150000", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
