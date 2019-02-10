61da7b1b8a9d3413b75a486be4d2d47f1caa64b7
muka <mukasc@gmail.com>
2019-02-10 15:34:22 -0200

    Salvando o primeiro arquivo no Amazon S3

--- src/main/java/com/muka/cursomc/CursomcApplication.java
+++ src/main/java/com/muka/cursomc/CursomcApplication.java
@@ -1,12 +1,18 @@
 package com.muka.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.muka.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
-		
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -14,7 +20,7 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		s3Service.uploadFile("C:\\Users\\User\\Pictures\\Saved Pictures\\WhatsApp Image 2018-06-28 at 15.41.01 (1).jpeg");
 		
 	}
 }
